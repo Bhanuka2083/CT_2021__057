fde7d4c78cb7dae44d9b0a6b4343e5d339c66c86
T M B M Thennakoon <bmaheesha2083@gmail.com>
2025-03-15 23:43:59 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("All Lab WorkSheets are in src folder...");
+        System.out.println("All Lab WorkSheets are in above folders...");
     }
 }
